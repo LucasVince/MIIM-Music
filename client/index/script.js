89bdf7aee87089e19185de873303c1476878be56
LucasVince <lucashenriquevince1714@gmail.com>
2024-12-06 22:11:15 -0300

    Added username on top of header

--- client/index/script.js
+++ client/index/script.js
@@ -1,9 +1,15 @@
 const token = JSON.parse(localStorage.getItem('token'));
 
+const headerUser = document.querySelector('#username');
+
 if (token) {
     const splitToken = token.split('.')[1];
     const playloadToken = JSON.parse(atob(splitToken));
+
     localStorage.setItem('clientId', JSON.stringify(playloadToken.id));
     localStorage.setItem('clientUsername', JSON.stringify(playloadToken.username));
+
     console.log(playloadToken);
+    
+    headerUser.innerHTML = playloadToken.username;
 }
